76da045f9bbd2fa3fdc18d01f5bfe0e4905c6e7d
Simon Laureti <simon.laureti@gmail.com>
2019-09-17 16:47:41 -0400

    add personne 1 info

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -1,9 +1,9 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
-    public static int studentNo = 338299;
-    public static String description = "Likes to take pictures";
+    public static String firstName = "Simon";
+    public static String lastName = "Laureti";
+    public static int studentNo = 300043861;
+    public static String description = "Here I am!";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
